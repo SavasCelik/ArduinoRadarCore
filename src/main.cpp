3070975aa238b302c19e7bf798ff4e7b856cedcc
Philipp Schmidd <welaya_luti8@outlook.com>
2023-02-27 20:30:32 +0100

    Make radar go back and forth

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
-#include <Radar.h>
+#include "config.h"
+#include "Radar.h"
 
-Radar myRadar;
+Radar myRadar(ECHO_PIN, TRIGGER_PIN, SERVO_PIN);
+int angleInDegrees = 0;
 
 void setup() {
+    myRadar.Setup();
+    myRadar.SetAngle(angleInDegrees);
 }
 
 void loop() {
+    for (angleInDegrees = 180; angleInDegrees >= 0; angleInDegrees--) { // goes from 180 degrees to 0 degrees
+    myRadar.SetAngle(angleInDegrees);
+    Serial.println(angleInDegrees);
+  }
+
+  for (angleInDegrees = 0; angleInDegrees <= 180; angleInDegrees++) {   // goes from 0 degrees to 180 degrees
+    myRadar.SetAngle(angleInDegrees);
+    Serial.println(angleInDegrees);
+  }
+    
 }
